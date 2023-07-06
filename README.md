015222322f6ee9e8dde71fea58e95c66b8b8edbd
rayanramoul <raysamram@gmail.com>
2023-07-07 00:10:27 +0200

    :sparkles: initial working commit for falcon inference

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # falcon_llm_streamlit
+
+You need to have setup this :
+```
+git clone https://github.com/cmp-nct/ggllm.cpp
+cd ggllm.cpp
+rm -rf build && mkdir build && cd build && cmake -DGGML_CUBLAS=1 .. && cmake --build . --config Release
+```
+
+## how to run GGLLM instructions :
+```
+bin/falcon_main -t 8 -ngl 100 -b 1 -m falcon7b-instruct.ggmlv3.q4_0.bin -p "What is a falcon?\n### Response:"
+```
--- app.py
+++ app.py
@@ -1,29 +1,36 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import transformers
-import torch
+#!/usr/bin/python
 
-# model = "tiiuae/falcon-7b-instruct"
-model_id="tiiuae/falcon-7b-instruct"
-tokenizer=AutoTokenizer.from_pretrained(model_id)
-model=AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
+import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+# import subprocess
+import os
 
 
-# tokenizer = AutoTokenizer.from_pretrained(model)
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    device_map="auto",
-)
-sequences = pipeline(
-   "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
-    max_length=200,
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-)
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+message_history = []
+
+st.set_page_config(page_title="Falcon LLM")
+potential_models = ['falcon7b-instruct.ggmlv3.q4_0.bin']
+
+def generate_response(prompt):
+    print(f"\n\n\nNEW PROMPT {prompt}\n\n\n")
+    args = ("ggllm.cpp/build/bin/falcon_main", "-t", "8", "-ngl", "100", "-b", "1", "-m", "/home/rayanramoul/models/falcon7b-instruct.ggmlv3.q4_0.bin", "-p",f'"{prompt}"')
+    # popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    # popen.wait()
+    # output = popen.stdout.read()
+    cmd = ' '.join(args)
+    output = os.popen(cmd).read()
+    print(f"\n\n\nOUTPUT {output}\n\n\n")
+    return str(output)
+
+agent_tone = st.selectbox('Agent Tone', ['Friendly', 'Neutral', 'Hostile', 'Introvert', 'Extrovert'])
+first_model = st.selectbox()
+prompt = st.text_input('Prompt', value='Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:')
+if st.button('Generate'):
+    result = generate_response(prompt)
+    st.write(result)
+
+
+st.write('Made with ❤️ by Rayan Samy Ramoul')
